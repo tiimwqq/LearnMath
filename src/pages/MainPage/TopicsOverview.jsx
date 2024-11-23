bc9d355fa8f7cb13c321429d61e854ce35f3d600
unknown <tiimwqq@gmail.com>
2024-11-24 01:36:16 +0500

    добавил секцию с заголовком и предисловием. добавил ховеры на шапку. создал секцию тем

---
+++ src/pages/MainPage/TopicsOverview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopicsOverview = () => {
+	return (
+		<div className='topics'>
+			<h3 className='topics__title'>Темы по разделам:</h3>
+		</div>
+	);
+};
+
+export default TopicsOverview;
---
+++ src/pages/MainPage/index.jsx
@@ -0,0 +1,13 @@
+import HeroSection from "./HeroSection";
+import TopicsOverview from "./TopicsOverview";
+
+const MainPage = () => {
+	return (
+		<div className='hero'>
+			<HeroSection />
+			<TopicsOverview/>
+		</div>
+	);
+};
+
+export default MainPage;
